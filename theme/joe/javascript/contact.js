df6c5c1ab4db56aeacdb3097529b40c6f3c9f73f
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-06 15:25:24 -0500

    More styling form tweaks:

--- theme/joe/javascript/contact.js
+++ theme/joe/javascript/contact.js
@@ -1,1 +1,30 @@
-contact.js
+//JS side of the homepage contact form - validation
+$(function() {
+	$(".error").hide();
+  $(".button").click(function() {
+  
+    $('.error').hide();
+    var name = $("input#name").val();
+      if (name == "") {
+      $("label#name_error").show();
+      $("input#name").focus();
+      return false;
+    }
+      var email = $("input#email").val();
+      if (email == "") {
+      $("label#email_error").show();
+      $("input#email").focus();
+      return false;
+    }
+      var message = $("textarea").val();
+      if (message == "") {
+      $("label#message_error").show();
+      $("textarea").focus();
+      return false;
+    }
+
+
+
+
+  });
+});
