532548478af91e0c54d9c9ed21fb5fda74deefca
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-10 05:04:25 -0500

    Adds error checking to mail script

--- email.php
+++ email.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-if(mail($toemail, 'New portfolio site message', $message, 'From: ' . $email)) {
+if(@mail($toemail, 'New portfolio site message', $message, 'From: ' . $email)) {
 	echo 'Your email was sent successfully.';
 } else {
 	echo 'There was a problem sending your email.';
